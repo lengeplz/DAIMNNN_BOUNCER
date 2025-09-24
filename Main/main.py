1506cefc59cda8cc5073174e3919816903a98ce5
Andre <s3953146@student.rmit.edu.au>
2025-09-25 04:55:43 +1000

    Added sound and initialisation code

---
+++ Main/main.py
@@ -0,0 +1,14 @@
+import sys
+import random
+from pathlib import Path
+
+import pygame
+
+# ---------- Paths ----------
+ROOT = Path(__file__).resolve().parent
+DATA = ROOT / "data"
+
+LOGO_FILE = DATA / "Daim.png"
+BG_FILE = DATA / "windows_XP.jpg"
+SOUND_FILE = DATA / "sound.mp3"
+
